58e502bbd00adaee1fca2c5951e8e7256e81fb99
saketh-dr1 <sakethdonthireddy1@gmail.com>
2026-06-18 13:50:49 +0530

    greeting with a name

---
+++ week1/greeting.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go DSR")
+		os.Exit(1)
+	}
+
+	name := os.Args[1]
+	fmt.Printf("Hello, %s, Welcome to backend engineering.\n", name)
+}
